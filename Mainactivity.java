e3746d751337b7a5a18b93b716a51026530dc5dd
papjm <102404994+papjm@users.noreply.github.com>
2023-06-06 21:51:10 +0100

    Update Mainactivity.java

--- Mainactivity.java
+++ Mainactivity.java
@@ -1,11 +1,24 @@
-package com.example.myapplication
+package com.example.myapplication;
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        EditText editTextName = findViewById(R.id.edtTxtName)
+
+        
+    }
+    
+    public void onBtnClick (View view) {
+        TextView txtHello = findViewById(R.id.txtMessage);
+        txtHello.setText("Hello");
     }
 }
