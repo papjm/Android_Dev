9c1b2eea1754ad58f0009326c7fe002a99d14c19
papjm <102404994+papjm@users.noreply.github.com>
2023-06-07 15:08:10 +0100

    Update hello.java

--- hello.java
+++ hello.java
@@ -2,7 +2,12 @@ package com.davinci.helloworld;
 
 public class hello {
     public static void main(String[] args) {
-        System.out.println("hello world");
+//        System.out.println("hello world");
+//        System.out.print("hello world");
+        int number = -5;
+        System.out.print(number);
+        long myLong = 5;
+        System.out.println(myLong);
+
     }
 }
-
