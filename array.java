bedff4e8ecada31d60fad4e3adc884eac53ee1f2
papjm <102404994+papjm@users.noreply.github.com>
2023-06-12 16:57:19 +0100

    Create array.java

---
+++ array.java
@@ -0,0 +1,11 @@
+package com.davinci.helloworld;
+
+public class hello {
+    public static void main(String[] args) {
+        String[] students = new String[5];
+
+        students[1] = "Da_Vinci";
+        students[0] = "Hackeone"
+
+    }
+}
