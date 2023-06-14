cbc018f519d5bcc5c41e16ae61a1038e368452f6
papjm <102404994+papjm@users.noreply.github.com>
2023-06-14 09:33:26 +0100

    Create animal.java

---
+++ animal.java
@@ -0,0 +1,15 @@
+package com.davinci.helloworld;
+
+public class animal {
+    private String name;
+    private String color;
+    private int legs;
+    private boolean hasTale;
+
+    public animal(String name, String color, int legs, boolean hasTale) {
+        this.name = name;
+        this.color = color;
+        this.legs = legs;
+        this.hasTale = hasTale;
+    }
+}
